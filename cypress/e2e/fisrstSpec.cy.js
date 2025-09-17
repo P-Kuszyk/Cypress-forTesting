5db10caee4a1124ded29258f595620eefe01f3b0
P-Kuszyk <p.kuszyk1221@wp.pl>
2025-09-17 16:03:34 +0200

    Add new test and change the name of the rest of it

--- cypress/e2e/fisrstSpec.cy.js
+++ cypress/e2e/fisrstSpec.cy.js
@@ -1,12 +1,12 @@
 const linkToWebsite = "/Website/index.html";
 
-describe('#1 Attempt', () => {
+describe('#1 Open the website', () => {
   it('Check the websie', () => {
     cy.visit('/Website/index.html')
   })
 })
 
-describe("#2 Attempt", () => {
+describe("#2 Title of the website", () => {
 it('Check the title of the site', () => {
   cy.visit(linkToWebsite) 
   cy.title().should('include', 'Testing')
@@ -17,7 +17,7 @@ it('Check if the title of the site is wrong', () =>{
 })
 })
 
-describe('#3 Attempt', () => {
+describe('#3 Buttons', () => {
   it('Check buttons', () => {
     cy.visit(linkToWebsite)
     //cy.viewport(1920, 1080) // Ustawienie odpowiedniej rozdzielczości | ZMIANA W CYPRESS.CONFIG.JS 
@@ -26,10 +26,24 @@ describe('#3 Attempt', () => {
   })
 })
 
-describe('#4 Attempt', ()=> {
+describe('#4 Visit register page', ()=> {
   it('Back to Home Page', () => {
     cy.visit('/Website/register.html')
     cy.get('#SG').click() // Wyszukiwanie elementu po ID 
     cy.url().should('include', linkToWebsite)
+  })
+})
+
+describe('#5 Textarea', () => {
+  it('Check if the textarea is empty', () => {
+    cy.visit(linkToWebsite)
+    cy.get('.textA').should('be.empty')
+  }),
+  it('Put something into textarea and clean after it', () => {
+    cy.visit(linkToWebsite)
+    cy.get('.textA').type("Random text")
+    cy.get('.textA').invoke('val').should('include', 'Random')
+    cy.get('.btnDelete').click()
+    cy.get('.textA').should('be.empty')
   })
 })
