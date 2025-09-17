47571a57dc120f059be0806fbc8bfecef41e0488
P-Kuszyk <p.kuszyk1221@wp.pl>
2025-09-17 15:47:33 +0200

    Changes of the website

--- Website/script.js
+++ Website/script.js
@@ -1,3 +1,15 @@
 const btnDelete = document.querySelector(".btnDelete")
-const btnSave = document.querySelectorAll(".btnSave")
+const btnSave = document.querySelector(".btnSave")
+const textArea = document.querySelector(".textA")
 
+btnDelete.addEventListener("click", () => {
+    textArea.value = "";
+    localStorage.clear();
+})
+
+btnSave.addEventListener("click", () => {
+    localStorage.setItem("Text", textArea.value);
+    console.log("Zapisano.")
+})
+
+textArea.value = localStorage.getItem("Text")
