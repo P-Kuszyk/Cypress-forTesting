c674e3074344662bcda73be3b0c6d6a890b245fb
P-Kuszyk <p.kuszyk1221@wp.pl>
2025-09-18 11:26:59 +0200

    Add tests and new pages

--- cypress/e2e/fisrstSpec.cy.js
+++ cypress/e2e/fisrstSpec.cy.js
@@ -18,18 +18,29 @@ it('Check if the title of the site is wrong', () =>{
 })
 
 describe('#3 Buttons', () => {
-  it('Check buttons', () => {
+  it('Check button register', () => {
     cy.visit(linkToWebsite)
     //cy.viewport(1920, 1080) // Ustawienie odpowiedniej rozdzielczości | ZMIANA W CYPRESS.CONFIG.JS 
     cy.get('.button.is-primary').click()
     cy.url().should('include', '/Website/register.html')
   })
+  it('Check button log in', () => {
+    cy.visit(linkToWebsite)
+    cy.get('.button.is-light').click()
+    cy.url().should('include', '/Website/login.html')
+  })
 })
 
-describe('#4 Visit register page', ()=> {
-  it('Back to Home Page', () => {
+describe('#4 Register page', ()=> {
+  it('Visit the register page', () => {
     cy.visit('/Website/register.html')
     cy.get('#SG').click() // Wyszukiwanie elementu po ID 
+  })
+  it('Back to home page', () => {
+    cy.visit(linkToWebsite)
+    cy.get('.button.is-primary').click()
+    cy.url().should('include', '/Website/register.html')
+    cy.get('#SG').click()
     cy.url().should('include', linkToWebsite)
   })
 })
@@ -43,7 +54,21 @@ describe('#5 Textarea', () => {
     cy.visit(linkToWebsite)
     cy.get('.textA').type("Random text")
     cy.get('.textA').invoke('val').should('include', 'Random')
+    /* 
+    invoke('text') → wywoła metodę .text() z jQuery i zwróci tekst.
+    invoke('val') → wywoła metodę .val() i zwróci wartość inputa.
+    invoke('attr', 'href') → wywoła metodę .attr('href') i zwróci wartość atrybutu.
+    */
     cy.get('.btnDelete').click()
     cy.get('.textA').should('be.empty')
+  }),
+  it('Chec if the text stay in textarea after refresh', () => {
+    cy.visit(linkToWebsite)
+    cy.get('.textA').type('Random text')
+    cy.get('.textA').invoke('val').should('include', 'Random')
+    cy.get('.btnSave').click()
+    cy.reload()
+    cy.get('.textA').invoke('val').should('not.be.empty')
+    cy.get('.btnDelete').click()
   })
 })
